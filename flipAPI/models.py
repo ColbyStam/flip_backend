61d65bdda2423a79c273a3c49475a231e15e0c4b
Ubuntu <ubuntu@ip-172-31-18-162.us-west-1.compute.internal>
2024-04-17 20:06:03 +0000

    backend4_17_24

--- flipAPI/models.py
+++ flipAPI/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+class FlashCard(models.Model):
+    
+    flashcard_id = models.AutoField()
+    set_id = models.AutoField()
+    front_text = models.TextField()
+    back_text = models.TextField()
+    created_at = models.DateTimeField()
+    
+    
+    
+class Flashcard_Set(models.Model):
+    
+    set_id = models.AutoField()
+    user_id = models.AutoField()
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField()
+    is_public = models.BooleanField()
+    
+    
+    
+class User(models.Model):
+    
+    user_id = models.AutoField()
+    username = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=200)
+    created_at = models.DateTimeField()
+    last_login = models.DateTimeField()
+ 
+ 
+ 
+class User_Flashcard_Set(models.Model):
+    
+    user_id = models.ForeignKey(to=User, on_delete=True)
+    set_id = models.ForeignKey(to=Flashcard_Set, on_delete=True)
+    is_favorite = models.BooleanField()
+    last_studied = models.DateTimeField()
+    
+    
+    
+class Study_Session(models.Model):
+    
+    session_id = models.AutoField()
+    user_id = models.ForeignKey(to=User, on_delete=True)
+    set_id = models.ForeignKey(to=Flashcard_Set, on_delete=True)
+    started_at = models.DateTimeField()
+    ended_at = models.DateTimeField()
+    score = models.AutoField()
